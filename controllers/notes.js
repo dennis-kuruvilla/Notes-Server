5f27b716b4388db27bf4ba5674f47b0858ee56f1
Dennis Kuruvilla <denniskuruvilla777@gmail.com>
2021-10-10 21:01:25 +0530

    upto part 4 + fixed test cases

--- controllers/notes.js
+++ controllers/notes.js
@@ -1,12 +1,27 @@
 const notesRouter = require('express').Router()
+const jwt = require('jsonwebtoken')
 const Note = require('../models/note')
+const User = require('../models/user')
 
+  //USING THEN IN THE FOLLOWING FUNCTION, REPLACED BY ASYNC AWAIT
+  // notesRouter.get('/', (request, response) => {
+  //   Note.find({}).then(notes => {
+  //     response.json(notes)
+  //   })
+  // })
 
-  notesRouter.get('/', (request, response) => {
-    Note.find({}).then(notes => {
-      response.json(notes)
-    })
-  })
+  //modified the following to show the user info of the notes 
+  // notesRouter.get('/', async (request, response) => { 
+  //   const notes = await Note.find({})
+  //   response.json(notes)
+  // })
+
+  notesRouter.get('/', async (request, response) => {
+    const notes = await Note
+      .find({}).populate('user', { username: 1, name: 1 })
+  
+    response.json(notes)
+  });
   
   //BEFORE MONGODB
   // app.get('/api/notes/:id', (request, response) => {
@@ -20,16 +35,28 @@ const Note = require('../models/note')
   //       }
   //     })
   
-  notesRouter.get('/:id', (request, response, next) => {
-    Note.findById(request.params.id)
-      .then(note => {
-        if (note) {
-          response.json(note)
-        } else {
-          response.status(404).end()
-        }
-      })
-      .catch(error => next(error))
+  //GET A SINGLE NOTE USING THEN()
+  // notesRouter.get('/:id', (request, response, next) => {
+  //   Note.findById(request.params.id)
+  //     .then(note => {
+  //       if (note) {
+  //         response.json(note)
+  //       } else {
+  //         response.status(404).end()
+  //       }
+  //     })
+  //     .catch(error => next(error))
+  // })
+
+  //USING ASYNC/AWAIT
+  notesRouter.get('/:id', async (request, response) => {
+    const note = await Note.findById(request.params.id)
+    if (note) {
+      response.json(note)
+    } else {
+      response.status(404).end()
+    }
+    // WITH THE HELP OF express-async-errors LIBRARY,  If an exception occurs in a async route, the execution is automatically passed to the error handling middleware.
   })
   
   // app.delete('/api/notes/:id', (request, response) => {
@@ -39,12 +66,18 @@ const Note = require('../models/note')
   //   response.status(204).end()
   // })
   
-  notesRouter.delete('/:id', (request, response, next) => {
-    Note.findByIdAndRemove(request.params.id)
-      .then(result => {
-        response.status(204).end()
-      })
-      .catch(error => next(error))
+  // USING THEN()
+  // notesRouter.delete('/:id', (request, response, next) => {
+  //   Note.findByIdAndRemove(request.params.id)
+  //     .then(result => {
+  //       response.status(204).end()
+  //     })
+  //     .catch(error => next(error))
+  // })
+
+  notesRouter.delete('/:id', async (request, response) => {
+    await Note.findByIdAndRemove(request.params.id)
+    response.status(204).end()
   })
   
   
@@ -77,23 +110,99 @@ const Note = require('../models/note')
   //   response.json(note)
   // })
   
-  notesRouter.post('/', (request, response) => {
-    const body = request.body
+  //POST USING THEN() - 
+  // notesRouter.post('/', (request, response, next) => {
+  //   const body = request.body
   
-    if (body.content === undefined) {
-      return response.status(400).json({ error: 'content missing' })
+  //   const note = new Note({
+  //     content: body.content,
+  //     important: body.important || false,
+  //     date: new Date(),
+  //   })
+  
+  //   note.save().then(savedNote => {
+  //     response.json(savedNote)
+  //   })
+  //   .catch(error => next(error))
+  // })
+
+  //identifying the authentication scheme used. here we are using the 'bearer' authentication scheme.
+  //and  Identifying the scheme tells the server how the attached credentials should be interpreted.
+  //this function  isolates the token from the authorization header
+  const getTokenFrom = request => {
+    const authorization = request.get('authorization')
+    if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+      return authorization.substring(7)
     }
-  
+    return null
+  }
+
+  notesRouter.post('/', async (request, response) => {
+
+    const body = request.body
+    
+    const token = getTokenFrom(request)
+    const decodedToken = jwt.verify(token, process.env.SECRET)
+    if (!token || !decodedToken.id) {
+      return response.status(401).json({ error: 'token missing or invalid' })
+    }
+
+    //getting the document of the user who createed the note
+    const user = await User.findById(decodedToken.id)
+
+    // console.log("userid: ",user._id)
+    
+    // console.log("creating note")
+
     const note = new Note({
       content: body.content,
       important: body.important || false,
       date: new Date(),
+      //adding the id of the user in the note object
+      user: user._id
     })
-  
-    note.save().then(savedNote => {
-      response.json(savedNote)
-    })
+
+    // console.log("created note")
+    
+    
+    const savedNote = await note.save()
+    
+    //updating the user's document by concatenating the note id to the array of note id's in the user document
+    // console.log("savednotes._id=",savedNote._id)
+    user.notes = user.notes.concat(savedNote._id)
+    // console.log("user notes=",user.notes)
+    // console.log("updated user notes")
+    
+    await user.save()
+
+    // console.log("saved updated user")
+
+    response.json(savedNote)
   })
+
+  // notesRouter.post('/', async (request, response, next) => {
+  //   const body = request.body
+  
+  //   const user = await User.findById(body.userId)
+  
+  //   const note = new Note({
+  //     content: body.content,
+  //     important: body.important === undefined ? false : body.important,
+  //     date: new Date(),
+  //     user: user._id
+  //   })
+  
+  //   const savedNote = await note.save()
+  //   user.notes = user.notes.concat(savedNote._id)
+  //   console.log("user notes=",user.notes)
+  //   console.log("updated user notes")
+  //   await user.save()
+  
+  //   response.json(savedNote.toJSON())
+  // })
+  
+
+
   
   notesRouter.put('/:id', (request, response, next) => {
     const body = request.body
--- utils/middleware.js
+++ utils/middleware.js
@@ -18,7 +18,16 @@ const errorHandler = (error, request, response, next) => {
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
   } else if (error.name === 'ValidationError') {
+    console.log("validation error")
     return response.status(400).json({ error: error.message })
+  } else if (error.name === 'JsonWebTokenError') {
+    return response.status(401).json({
+      error: 'invalid token'
+    })
+  } else if (error.name === 'TokenExpiredError') {
+    return response.status(401).json({
+      error: 'token expired'
+    })
   }
 
   next(error)
